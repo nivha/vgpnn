cf64363cd5d55320f3ffdc83ef2bf7ecf1cc4e2d
nivha <nivha@users.noreply.github.com>
2022-10-02 09:51:38 +0300

    updated index

--- static/js/index.js
+++ static/js/index.js
@@ -10,7 +10,34 @@ $(document).ready(function() {
 
     });
 
-		// <--results-carousel-->
+		
+		// <-- I'm Vegeterian button-->
+		$('button#button').click(function() {
+			// toggle style (color)
+			$(this).toggleClass("is-primary");
+
+			// change image
+			var img = document.getElementById("eww");
+			var isMeat = img.src.indexOf('eww.jpg') != -1;
+			img.src = isMeat ? img.src.replace('eww.jpg', 'drake.png') : img.src.replace('drake.png', 'eww.jpg');
+			
+			// change videos
+			const meats = ["waterfall2meat", "lava2meat", "meat", "sheep2meat", "meat2waterfall", "meat2lava", "meat2sheep"]
+			const slims = ["waterfall2slime", "lava2slime", "slime", "sheep2slime", "slime2waterfall", "slime2lava", "slime2sheep"]
+			meats.forEach(function (mn, index) {
+				sn = slims[index];
+				var vid = document.getElementById(mn);
+				var src = vid.getElementsByTagName('source')[0].src
+				var isMeat = src.indexOf(mn) != -1;
+				vid.getElementsByTagName('source')[0].src = isMeat ? src.replace(mn, sn) : src.replace(sn, mn);
+				vid.load()				
+		});
+		// <--/ I'm Vegeterian button-->
+			
+			
+		});
+
+		// <--generation-carousel-->
     var options = {
 			slidesToScroll: 1,
 			slidesToShow: 1,
@@ -30,30 +57,7 @@ $(document).ready(function() {
     		console.log(state);
     	});
     }
-		// </--results-carousel-->
-		
-		// <--analogies-carousel-->
-		var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: false,
-			autoplaySpeed: 3000,
-    }
-
-	  // Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.analogies-carousel', options);
-
-    // Loop on each carousel initialized
-    for(var i = 0; i < carousels.length; i++) {
-    	// Add listener to  event
-    	carousels[i].on('before:show', state => {
-    		console.log(state);
-    	});
-    }
-		// </--analogies-carousel-->
-
+		// </--generation-carousel-->
 		
 		// <--spatret-carousel-->
 		var options = {
@@ -76,6 +80,29 @@ $(document).ready(function() {
     	});
     }
 		// </--spatret-carousel-->
+
+
+		// <--comparison-carousel-->
+		var options = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
+    }
+
+	  // Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.comparison-carousel', options);
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+    	// Add listener to  event
+    	carousels[i].on('before:show', state => {
+    		console.log(state);
+    	});
+    }
+		// </--comparison-carousel-->
 
 		
 		// <--tempret-carousel-->
@@ -101,6 +128,29 @@ $(document).ready(function() {
 		// </--tempret-carousel-->
 
 
+		// <--sumret-carousel-->
+		var options = {
+			slidesToScroll: 1,
+			slidesToShow: 3,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
+    }
+
+	  // Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.sumret-carousel', options);
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+    	// Add listener to  event
+    	carousels[i].on('before:show', state => {
+    		console.log(state);
+    	});
+    }
+		// </--sumret-carousel-->
+
+
 		// <--cinp-carousel-->
 		var options = {
 			slidesToScroll: 1,
@@ -123,6 +173,28 @@ $(document).ready(function() {
     }
 		// </--cinp-carousel-->
 
+		// <--analogies-carousel-->
+		var options = {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			loop: true,
+			infinite: true,
+			autoplay: false,
+			autoplaySpeed: 3000,
+    }
+
+	  // Initialize all div with carousel class
+    var carousels = bulmaCarousel.attach('.analogies-carousel', options);
+
+    // Loop on each carousel initialized
+    for(var i = 0; i < carousels.length; i++) {
+    	// Add listener to  event
+    	carousels[i].on('before:show', state => {
+    		console.log(state);
+    	});
+    }
+		// </--analogies-carousel-->
+
 
 
     // Access to bulmaCarousel instance of an element
@@ -136,9 +208,23 @@ $(document).ready(function() {
 		
 		
 		// fix analogies sheep width
-		// w_height = $(".analogies_vid, .analogies_w")[0].getBoundingClientRect().height;
-		// $(".analogies_vid, .analogies_h").css("width", w_height);
-		// $(".analogies_vid, .analogies_w").css("height", w_height);
+		w_height = $(".analogies_vid, .analogies_w")[0].getBoundingClientRect().height;
+		$(".analogies_vid, .analogies_h").css("width", w_height);
+		$(".analogies_vid, .analogies_w").css("height", w_height);
+		
+		// add Next-Prev to sliders
+		
+		nexts = $(".slider-navigation-next");
+		for(var i=0;i<nexts.length;i++){
+				nexts[i].innerHTML += "<b>Next</b>";
+		}
+		prevs = $(".slider-navigation-previous");
+		for(var i=0;i<prevs.length;i++){
+				prevs[i].innerHTML += "<b>Prev</b>";
+		}
+
+		// $(".slider-navigation-next")[0].innerHTML += "Next";
+		// $(".slider-navigation-previous")[0].innerHTML += "Prev";
 
 
 })
